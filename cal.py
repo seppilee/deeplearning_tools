1e2e600c5d9b5f2a3f2395340564dfe368b89f7c
cuba <seppi.lee@hanmail.net>
2019-02-04 00:17:18 +0900

    module

---
+++ cal.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+ 
+def substract(a, b):
+    return a - b
+
+def main():
+    a = add(1,2)
+    s = substract(a,1)
+    print(s)
+
+if __name__ == '__main__':
+    main()
