6686f49f81192c6f4c0574b8a332de489a18e305
cuba <seppi.lee@hanmail.net>
2019-02-12 23:25:16 +0900

    intro

--- airtravel.py
+++ airtravel.py
@@ -1,6 +1,13 @@
+
 class Flight:
-    def __init__(self, number):
-        self._number = number
+
+    def __init__(self):
+        print('init')
+        super().__init__()
+
+    def __new__(cls):
+        print('new')
+        return super().__new__(cls)
 
     def number(self):
-        return self._number
+        return 'SN060'
